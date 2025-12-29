558a2aae62dec22ee62e07331d16b9ea6acb5080
Henry Foster <henryfoster013@gmail.com>
2025-12-29 22:55:01 +0000

    Added a bunch of advertisements, reworked some meshes and rewrote the global shader. Dubbed it 'Henrys shader'. Also went ahead and cleaned up the file system

---
+++ Assets/Scripts/Monobehaviours/Desktop/DesktopManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static GenericUtils;
+using static SoundUtils;
+
+public class DesktopManager : MonoBehaviour{
+    
+    [Header(" - Startup - ")]
+    [SerializeField] GameObject FadeIn;
+    [SerializeField] SoundEffect StartSound;
+    [Header(" - Cursor - ")]
+    [SerializeField] RectTransform CursorRect;
+    [SerializeField] RectTransform CanvasRect;
+    [SerializeField] Camera UI_Camera;
+    [Header(" - Hooks - ")]
+    [SerializeField] Transform WindowHolder;
+    [SerializeField] Transform TabHolder;
+
+    void Start(){
+        PlaySFX(StartSound);
+        FadeIn.SetActive(true);
+    }
+
+    void Update(){
+        Cursor.visible = false;
+        Vector2 localPoint;
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(CanvasRect, Input.mousePosition, UI_Camera, out localPoint);
+        CursorRect.localPosition = localPoint;
+    }
+}
